064130bdfe7f9ddd2805ebea4aae01d5f055ed48
xusniya <118680313+xusniya@users.noreply.github.com>
2026-02-03 14:14:05 +0500

    Update __init__.py

--- apps/views/__init__.py
+++ apps/views/__init__.py
@@ -1,10 +1,41 @@
-from apps.views.user import UserListAPIView, UserCreateAPIView, DetailUserAPIView, UpdateUserAPIView, DeleteUserAPIView
-from apps.views.farmer import FarmerListAPIView, FarmerCreateAPIView, DetailFarmerAPIView, UpdateFarmerAPIView, \
-    DeleteFarmerAPIView
-from apps.views.buyer import BuyerListAPIView, BuyerCreateAPIView, DetailBuyerAPIView, UpdateBuyerAPIView, \
-    DeleteBuyerAPIView
-from apps.views.distributor import DistributorListAPIView, DistributorCreateAPIView, DetailDistributorAPIView, \
-    UpdateDistributorAPIView, DeleteDistributorAPIView
-from apps.views.supplier import SupplierListAPIView, SupplierCreateAPIView, DetailSupplierAPIView, \
-    UpdateSupplierAPIView, DeleteSupplierAPIView
-from apps.views.health import health
+from apps.views.user import (
+    UserListAPIView as UserListAPIView,
+    UserCreateAPIView as UserCreateAPIView,
+    DetailUserAPIView as DetailUserAPIView,
+    UpdateUserAPIView as UpdateUserAPIView,
+    DeleteUserAPIView as DeleteUserAPIView,
+)
+
+from apps.views.farmer import (
+    FarmerListAPIView as FarmerListAPIView,
+    FarmerCreateAPIView as FarmerCreateAPIView,
+    DetailFarmerAPIView as DetailFarmerAPIView,
+    UpdateFarmerAPIView as UpdateFarmerAPIView,
+    DeleteFarmerAPIView as DeleteFarmerAPIView,
+)
+
+from apps.views.buyer import (
+    BuyerListAPIView as BuyerListAPIView,
+    BuyerCreateAPIView as BuyerCreateAPIView,
+    DetailBuyerAPIView as DetailBuyerAPIView,
+    UpdateBuyerAPIView as UpdateBuyerAPIView,
+    DeleteBuyerAPIView as DeleteBuyerAPIView,
+)
+
+from apps.views.distributor import (
+    DistributorListAPIView as DistributorListAPIView,
+    DistributorCreateAPIView as DistributorCreateAPIView,
+    DetailDistributorAPIView as DetailDistributorAPIView,
+    UpdateDistributorAPIView as UpdateDistributorAPIView,
+    DeleteDistributorAPIView as DeleteDistributorAPIView,
+)
+
+from apps.views.supplier import (
+    SupplierListAPIView as SupplierListAPIView,
+    SupplierCreateAPIView as SupplierCreateAPIView,
+    DetailSupplierAPIView as DetailSupplierAPIView,
+    UpdateSupplierAPIView as UpdateSupplierAPIView,
+    DeleteSupplierAPIView as DeleteSupplierAPIView,
+)
+
+from apps.views.health import health as health
